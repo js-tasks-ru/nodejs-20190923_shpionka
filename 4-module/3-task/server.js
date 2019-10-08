7c7e55ccb33553fbfd2db962b4450ce8046701b5
shpionka <topchylo@gmail.com>
2019-10-08 20:08:43 +0200

    add solution for task 3 module 4

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,22 +1,40 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+const url = require("url");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on("request", (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filepath = path.join(__dirname, "files", pathname);
 
   switch (req.method) {
-    case 'DELETE':
+    case "DELETE":
+      if (pathname.indexOf("/") > 0) {
+        res.statusCode = 400;
+        res.end();
+      } else if (!fs.existsSync(filepath)) {
+        res.statusCode = 404;
+        res.end();
+      } else {
+        fs.unlink(filepath, error => {
+          if (error) {
+            res.statusCode = 500;
+            res.end("Error");
+          } else {
+            res.statusCode = 200;
+            res.end("Success");
+          }
+        });
+      }
 
       break;
 
     default:
       res.statusCode = 501;
-      res.end('Not implemented');
+      res.end("Not implemented");
   }
 });
 
