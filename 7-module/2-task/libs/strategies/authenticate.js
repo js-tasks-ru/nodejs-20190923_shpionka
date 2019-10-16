95e88c5a4d174b066d2a17479238a565a3d26157
shpionka <topchylo@gmail.com>
2019-10-16 22:14:46 +0200

    implement authenticate function

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,19 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+
+  if (email === undefined || email === null) {
+    return done(null, false, 'Не указан email');
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    await User.create({email: email, displayName: displayName}, function (err, createdUser) {
+      if (err) return done(err, false, 'Cant save user to db');
+      return done(null, createdUser);
+    });
+  } else {
+    return done(null, user);
+  }
 };
